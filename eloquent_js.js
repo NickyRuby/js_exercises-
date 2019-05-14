99f28da29e4bd63ac8bf9fa09843b949596e1568
NickyRuby <nikita.lobyntsev@gmail.com>
2019-05-14 17:41:42 +0300

    14 May
    High-Order Functions: Dominant direction.

--- eloquent_js.js
+++ eloquent_js.js
@@ -129,3 +129,53 @@ Remember that each script object has a direction property that can be
 The dominant direction is the direction of a majority of the characters that
 have a script associated with them. The characterScript and countBy
 functions defined earlier in the chapter are probably useful here.*/
+
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => {
+      return code >= from && code < to;
+    })) {
+      return script;
+    }
+  }
+  return null;
+}
+
+
+
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if (known == -1) {
+      counts.push({name, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
+
+
+function dominantDirection(string){
+  let scripts = countBy(string, char => {				// [name: "fsaf", count: n]
+    let script = characterScript(char.codePointAt(0));	// script
+    return script? script.direction : "none";
+  }).filter(({name}) => name != "none");
+
+
+ return scripts.reduce((a,b) => {
+   return a.count < b.count? b : a;
+ }).name;
+
+
+}
+
+
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// →
