07562b8d1a258445364a2b7870e0b66890dd29ed
NickyRuby <nikita.lobyntsev@gmail.com>
2019-06-01 10:01:30 +0300

    Groups

--- eloquent_js.js
+++ eloquent_js.js
@@ -220,3 +220,43 @@ console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 console.log(new Vec(3, 4).length);
 // → 5
 // → 5
+
+
+// Task 2: Groups
+
+class Group  {
+  constructor() {
+   this.members = [];
+  }
+
+  has(value) {
+    return this.members.includes(value);
+  }
+
+  add(value) {
+    if (!this.has(value)) this.members.push(value);
+  }
+
+  delete(value) {
+    this.members = this.members.filter(v => v !== value);
+  }
+
+  static from (array) {
+    let hey = new Group;
+    for (let each of array) {
+      hey.add(each);
+    }
+    return hey;
+  }
+}
+
+let group = Group.from([10, 20]);
+console.log(group);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
