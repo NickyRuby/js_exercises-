5d0eeffad6a06b85051e2374de1d0f9df6aa1735
NickyRuby <nikita.lobyntsev@gmail.com>
2019-06-03 18:50:25 +0300

    Iteratable groups

--- eloquent_js.js
+++ eloquent_js.js
@@ -260,3 +260,60 @@ group.add(10);
 group.delete(10);
 console.log(group.has(10));
 // → false
+
+
+// Task 3: Make Group Interatable
+
+class Group  {
+  constructor() {
+    this.members = [];
+  }
+
+  add(value) {
+    if (!this.has(value)) {
+    this.members.push(value);
+    }
+  }
+
+  delete(value) {
+    this.members = this.members.filter(v => v !== value);
+  }
+
+  has(value){
+    return this.members.includes(value);
+  }
+
+  next() {
+    if (this.count == this.members.length) return {done:true}
+
+    let value = { count: 0,
+				  value: this.members[this.count]
+                }
+    this.count++
+
+    return {value, done: false}
+  }
+
+  static from (obj) {
+    let group = new Group;
+    for (let each of obj) {
+      group.add(each);
+    }
+    return group;
+  }
+
+}
+
+let group = Group.from([10, 20]);
+console.log(group);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
+for (let value of Group.from(["a", "b", "c"])) {
+  console.log(value);
+}
